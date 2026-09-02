ca9fd4d82ecfdc17dbeb9945dedec6a1003da8dc
hrishita <hrishitajoshi86@gmail.com>
2026-09-03 00:44:07 +0530

    util response

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse {
+    constructor(statusCode, message = "Success", data) {
+        this.statusCode = statusCode;
+        this.success = statusCode < 400;
+        this.message = message;
+        this.data = data;
+    }
+}
+export default ApiResponse;
